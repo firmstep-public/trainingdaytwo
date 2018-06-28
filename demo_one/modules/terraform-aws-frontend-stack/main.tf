24d7b34ab0f01c8e81eaf78f9d578a93bcc99f04
Jamie Nelson <jamie@theinstillery.com>
2018-06-28 14:04:57 +0100

    Day two demo

--- demo_one/modules/terraform-aws-frontend-stack/main.tf
+++ demo_one/modules/terraform-aws-frontend-stack/main.tf
@@ -15,17 +15,11 @@ variable "vpc_id" {
 
 variable "subnet_ids" {
   description = "description"
-  default     = "description"
   type        = "list"
 }
 
 variable "ami_id" {
   description = ""
-}
-
-variable "security_group_ids" {
-  description = ""
-  type        = "list"
 }
 
 variable "tags" {
@@ -58,6 +52,16 @@ resource "aws_launch_template" "this" {
   name_prefix   = "${var.name}"
   image_id      = "${var.ami_id}"
   instance_type = "${var.instance_type}"
+
+  instance_market_options {
+    market_type = "spot"
+  }
+
+  monitoring {
+    enabled = true
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.this.id}"]
 }
 
 resource "aws_autoscaling_group" "this" {
@@ -70,4 +74,67 @@ resource "aws_autoscaling_group" "this" {
     id      = "${aws_launch_template.this.id}"
     version = "$$Latest"
   }
+}
+
+resource "aws_security_group" "this" {
+  vpc_id      = "${var.vpc_id}"
+  name_prefix = "${var.name}-instance-sg"
+
+  tags = "${var.tags}"
+}
+
+resource "aws_security_group" "this_elb" {
+  vpc_id      = "${var.vpc_id}"
+  name_prefix = "${var.name}-elb-sg"
+
+  tags = "${var.tags}"
+}
+
+resource "aws_security_group_rule" "allow_access_out_elb" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.this.id}"
+}
+
+resource "aws_security_group_rule" "allow_access_out_instance" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.this.id}"
+}
+
+resource "aws_security_group_rule" "allow_http_instance" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = ["${aws_security_group.this_elb.id}"]
+  security_group_id        = "${aws_security_group.this.id}"
+}
+
+resource "aws_security_group_rule" "allow_https_elb" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.this_elb.id}"
+}
+
+resource "aws_security_group_rule" "allow_http_elb" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.this_elb.id}"
+}
+
+output "security_group_id" {
+  value = "${aws_security_group.this.id}"
 }
